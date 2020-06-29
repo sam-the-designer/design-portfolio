424e816118743651532139d7db7df3fa8fde89fd
megan <50967388+mwinsby@users.noreply.github.com>
2020-06-28 22:28:46 -0700

    Update scrolling.js
    another try of the image load

--- js/scrolling.js
+++ js/scrolling.js
@@ -43,7 +43,7 @@ function setTeeth(){
 }
 
 function loadSnackArray(callback){
-  var snackFolder = "./images/snacks/";
+  var snackFolder = "images/snacks";
   $.ajax({
       url : snackFolder,
       success: function (data) {
@@ -62,7 +62,7 @@ function loadSnackArray(callback){
 }
 
 function loadResults(){
-  var resultFolder = "./images/results/";
+  var resultFolder = "./images/results";
   $.ajax({
       url : resultFolder,
       success: function (data) {
@@ -77,7 +77,7 @@ function loadResults(){
 }
 
 function loadLogoArray(callback){
-  var logoFolder = "./images/logos/";
+  var logoFolder = "/images/logos";
   $.ajax({
       url : logoFolder,
       success: function (data) {
